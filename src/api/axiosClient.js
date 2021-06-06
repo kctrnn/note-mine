ce594bd9de1cd51b723b1090e6ebdfe8f58a75e6
kctrnn <kctrnn@gmail.com>
2021-06-06 12:03:24 +0700

    Setup api, add axiosClient

---
+++ src/api/axiosClient.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import StorageKeys from 'constants/storage-keys';
+import queryString from 'query-string';
+
+const axiosClient = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    'content-type': 'application/json',
+  },
+  paramsSerializer: (params) => queryString.stringify(params),
+});
+
+// Add a request interceptor
+axios.interceptors.request.use(
+  (config) => {
+    const customHeaders = {};
+
+    const accessToken = localStorage.getItem(StorageKeys.TOKEN);
+    if (accessToken) {
+      customHeaders.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return {
+      ...config,
+      headers: {
+        ...customHeaders, // auto attach token
+        ...config.headers, // but you can override for some requests
+      },
+    };
+  },
+  (error) => Promise.reject(error)
+);
+
+// Add a response interceptor
+axios.interceptors.response.use(
+  (response) => {
+    if (response && response.data) {
+      return response.data;
+    }
+
+    return response;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default axiosClient;
